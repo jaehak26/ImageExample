2879a12b23786826cb6a2debc5087535c41393bd
jaehak26 <jack10829@naver.com>
2023-11-10 01:28:37 +0900

    helmet추가

--- back/server.js
+++ back/server.js
@@ -3,12 +3,14 @@ const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
-
+const helmet = require('helmet');
 const app = express();
 app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:5000'] }));
-//이걸 쓰면 uploads폴더를 http:localhost:port/uploads로 접근이 가능함
+//이걸 쓰면 uploads폴더를 http://localhost:port/uploads로 접근이 가능함
 //Not allowed to load local resource: file:///D:/react/ImageExample/back/uploads/*.jpg 에러 해결용
 app.use('/uploads', express.static('uploads'));
+app.use(helmet());
+
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
